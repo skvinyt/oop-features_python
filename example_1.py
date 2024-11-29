73c4f522af1224b38d80ccaf9bafe97123b0d8d1
skvinyt <skvinyt@yandex.ru>
2024-11-30 02:00:02 +0700

    Add examplr first

---
+++ example_1.py
@@ -0,0 +1,67 @@
+class Matrix:
+    def __init__(self, rows, cols):
+        self.rows = rows
+        self.cols = cols
+        self.data = [[0 for _ in range(cols)] for _ in range(rows)]
+
+    def __str__(self):
+        return '\n'.join([' '.join(map(str, row)) for row in self.data])
+
+    def add(self, other):
+        if self.rows != other.rows or self.cols != other.cols:
+            raise ValueError("Матрицы должны иметь одинаковые размеры для сложения")
+        result = Matrix(self.rows, self.cols)
+        for i in range(self.rows):
+            for j in range(self.cols):
+                result.data[i][j] = self.data[i][j] + other.data[i][j]
+        return result
+
+    def subtract(self, other):
+        if self.rows != other.rows or self.cols != other.cols:
+            raise ValueError("Матрицы должны иметь одинаковые размеры для вычитания")
+        result = Matrix(self.rows, self.cols)
+        for i in range(self.rows):
+            for j in range(self.cols):
+                result.data[i][j] = self.data[i][j] - other.data[i][j]
+        return result
+
+    def multiply(self, other):
+        if self.cols != other.rows:
+            raise ValueError("Количество столбцов первой матрицы должно совпадать с количеством строк второй матрицы")
+        result = Matrix(self.rows, other.cols)
+        for i in range(self.rows):
+            for j in range(other.cols):
+                result.data[i][j] = sum(self.data[i][k] * other.data[k][j] for k in range(self.cols))
+        return result
+
+    def transpose(self):
+        result = Matrix(self.cols, self.rows)
+        for i in range(self.rows):
+            for j in range(self.cols):
+                result.data[j][i] = self.data[i][j]
+        return result
+
+# Пример использования
+m1 = Matrix(2, 3)
+m1.data = [[11, 4, 7], [6, 15, 2]]
+m2 = Matrix(2, 3)
+m2.data = [[7, 14, 9], [8, 11, 10]]
+
+print("Матрица 1:")
+print(m1)
+print("Матрица 2:")
+print(m2)
+
+print("Сложение матриц:")
+print(m1.add(m2))
+
+print("Вычитание матриц:")
+print(m1.subtract(m2))
+
+m3 = Matrix(3, 2)
+m3.data = [[1, 2], [3, 4], [5, 6]]
+print("Умножение матриц:")
+print(m1.multiply(m3))
+
+print("Транспонирование матрицы 1:")
+print(m1.transpose())
